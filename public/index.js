1e69335d8be60289c6ec05a7c2814e8d370d4329
Chris_Velázquez04 <placebo0704@gmail.com>
2022-09-29 19:50:27 +0000

    desktop

--- public/index.js
+++ public/index.js
@@ -1,1 +1,14 @@
-//* === App E-commerce Javascript === */
+//*TODO === ===  App E-commerce Javascript === === */
+//** === vARIABLES === */
+const userIcon = document.querySelector(`#idNavUser`);
+const desktopMenu = document.querySelector("#idDesktopMenu");
+
+//** === Execute Code ===  */
+
+const upDesktop = () => {
+  if (desktopMenu) {
+    desktopMenu.classList.toggle(`active`);
+  }
+};
+
+userIcon.addEventListener("click", upDesktop);
